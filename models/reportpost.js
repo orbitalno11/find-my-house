4fc8846e625ac8699a30b02bdff6a26f86e53ae3
orbitalno11 <43870591+orbitalno11@users.noreply.github.com>
2019-05-24 19:23:30 +0700

    m

--- models/reportpost.js
+++ models/reportpost.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/FindMyHouse2');
-const connectString = 'mongodb://stn:<Sitnon@2541>@cluster0-mb8sl.gcp.mongodb.net/findmyhouse?retryWrites=true';
+const connectString = 'mongodb://stn:<Sitnon@2541>@cluster0-mb8sl.gcp.mongodb.net/findmyhouse';
 mongoose.connect(connectString);
 
 const Schema = mongoose.Schema;
